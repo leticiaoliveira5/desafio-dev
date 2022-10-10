f1ebfc9e8ca5cad10613bf4b8b9f68213e526f3a
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-10 19:50:52 -0300

    cria usuário no seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Usuário 
+User.create!(email: 'admin@app.net', password: '123456')
