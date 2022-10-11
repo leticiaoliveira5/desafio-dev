736ee228234504711cf8614737f8190571c2e0d5
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-11 00:37:30 -0300

    configura factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,5 +1,12 @@
+require 'faker'
+
 FactoryBot.define do
   factory :transaction do
-    
+    type { 0 }
+    sold_at { 2.days.ago }
+    amount { 100.50 }
+    document { '12345678910234' }
+    card { '1111****1111' }
+    store { store }
   end
 end
