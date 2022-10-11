438b2a9fe9d825b8e7dd10a4d3e9e0f4b8dc2a98
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-11 00:42:32 -0300

    atualiza validações

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,11 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   it { is_expected.to validate_presence_of(:type) }
-  it { is_expected.to validate_presence_of(:sold_at) }
   it { is_expected.to validate_presence_of(:amount) }
-  it { is_expected.to validate_presence_of(:document) }
-  it { is_expected.to validate_presence_of(:card) }
   it { is_expected.to belong_to(:store) }
+  it { is_expected.to belong_to(:user) }
   it { is_expected.to allow_values(:debit, :slip, :mortgage, :credit, :loan, 
                                    :sales, :ted, :doc, :rent ).for(:type) }
 end
