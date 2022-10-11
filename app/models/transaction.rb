4774032a1343f1d5be95a6b06f2b7018d68dc47e
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-10-11 06:13:17 -0300

    Implementa tabela para exibir os dados (#9)

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,4 +5,8 @@ class Transaction < ApplicationRecord
 
   enum type: { debit: 1, slip: 2, financing: 3, credit: 4, loan: 5,
                sales: 6, ted: 7, doc: 8, rent: 9 }
+
+  def value
+    type.in?(%w[debit credit loan sales ted doc]) ? amount : 0 - amount
+  end
 end
