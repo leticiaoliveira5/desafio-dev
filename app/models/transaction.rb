6f4dea7faaafad4aff085219dbd448dc0160fe89
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-11 00:52:08 -0300

    troca nome do tipo financiamento

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,6 +3,6 @@ class Transaction < ApplicationRecord
 
   validates :type, :amount, presence: true
 
-  enum type: { debit: 1, slip: 2, motgage: 3, credit: 4, loan: 5,
+  enum type: { debit: 1, slip: 2, financing: 3, credit: 4, loan: 5,
                sales: 6, ted: 7, doc: 8, rent: 9 }
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Transaction, type: :model do
   it { is_expected.to validate_presence_of(:type) }
   it { is_expected.to validate_presence_of(:amount) }
   it { is_expected.to belong_to(:store) }
-  it { is_expected.to belong_to(:user) }
-  it { is_expected.to allow_values(:debit, :slip, :mortgage, :credit, :loan, 
-                                   :sales, :ted, :doc, :rent ).for(:type) }
+  it do
+    expect(Transaction.new).to allow_values(:debit, :slip, :financing, :credit, :loan,
+                                    :sales, :ted, :doc, :rent).for(:type)
+  end
 end
