587a54e8b4cbe654e7e78cbbb5a37ef6ba28157f
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-11 02:07:30 -0300

    cria action para parsear arquivo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index; end
+
+  def parse_cnab
+    if params[:file]
+      Parser.new(file: params[:file], current_user: current_user).call
+    end
+  end
 end
