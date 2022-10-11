9f6b2d58c762efaf4451627210f65ef17f9d06db
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-11 00:30:15 -0300

    concerta teste

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -8,5 +8,5 @@ RSpec.describe Transaction, type: :model do
   it { is_expected.to validate_presence_of(:card) }
   it { is_expected.to belong_to(:store) }
   it { is_expected.to allow_values(:debit, :slip, :mortgage, :credit, :loan, 
-                                   :sales, :ted, :doc, :rent ).for(:role) }
+                                   :sales, :ted, :doc, :rent ).for(:type) }
 end
