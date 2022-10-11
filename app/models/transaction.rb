caf6e2cffa95a62f578b4c27946dade0d1a9d6d5
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-11 00:26:37 -0300

    adiciona validações e enum no model transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,9 @@
 class Transaction < ApplicationRecord
+  belong_to :store
+  belong_to :user, through: :store
+
+  validates :type, :sold_at, :amount, :document, :card presence: true
+
+  enum type: { debit: 1, slip: 2, motgage: 3, credit: 4, loan: 5,
+               sales: 6, ted: 7, doc: 8, rent: 9 }
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:type) }
+  it { is_expected.to validate_presence_of(:sold_at) }
+  it { is_expected.to validate_presence_of(:amount) }
+  it { is_expected.to validate_presence_of(:document) }
+  it { is_expected.to validate_presence_of(:card) }
+  it { is_expected.to belong_to(:store) }
+  it { is_expected.to allow_values(:debit, :slip, :mortgage, :credit, :loan, 
+                                   :sales, :ted, :doc, :rent ).for(:role) }
 end
