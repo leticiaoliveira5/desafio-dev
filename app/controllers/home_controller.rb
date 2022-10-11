4a24ea0822bc6c502cd912d1a27444047696a442
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-11 02:58:33 -0300

    controller retorna aviso para usuario

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,12 @@ class HomeController < ApplicationController
   def parse_cnab
     return unless params[:file]
     
-    Parser.new(file: params[:file], current_user: current_user).call
+    parser = Parser.new(file: params[:file], current_user: current_user)
+
+    if parser.valid? && parser.call
+      redirect_to root_path, notice: 'Os dados foram carregados com sucesso'
+    else
+      redirect_to root_path, alert: 'Houve um problema ao parsear o arquivo. Certifique-se de que o arquivo selecionado é válido.'
+    end
   end
 end
