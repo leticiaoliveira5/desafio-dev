22ec5d4d84655782cd3c67c51f25a8fbf13ea051
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-10-11 04:33:01 -0300

    Parseia arquivo (#8)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,16 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index; end
+
+  def parse_cnab
+    return unless params[:file]
+
+    parser = Parser.new(file: params[:file], current_user: current_user)
+
+    if parser.valid? && parser.call
+      redirect_to root_path, notice: t('.parser_success')
+    else
+      redirect_to root_path, alert: t('.parser_failure')
+    end
+  end
 end
