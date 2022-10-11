e16c892a97602c9673189d2ec3e5cf9051f8d1ae
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-11 03:43:50 -0300

    remove warnings rubocop

--- spec/services/parser_spec.rb
+++ spec/services/parser_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Parser, type: :service do
   let(:user) { create(:user) }
-  let(:file) { fixture_file_upload('spec/fixtures/CNAB.txt', 'text')  }
-  subject { described_class.new(file: file, current_user: user) }
+  let(:file) { fixture_file_upload('spec/fixtures/CNAB.txt', 'text/plain') }
+  let(:parser) { described_class.new(file: file, current_user: user) }
 
   describe '#call' do
     it 'insere os dados nas tabelas loja e transação' do
-      expect { subject.call }.to change(Transaction, :count).by(21)
+      expect { parser.call }.to change(Transaction, :count).by(21)
         .and change(Store, :count).by(5)
       expect(Transaction.first.type).to eq 'financing'
       expect(Store.first.user_id).to eq user.id
@@ -16,19 +16,19 @@ RSpec.describe Parser, type: :service do
 
   describe '#valid?' do
     context 'quando o formato do arquivo é txt' do
-      it { expect(subject.valid?).to be true }
+      it { expect(parser.valid?).to be true }
     end
 
     context 'quando o formato do arquivo não é txt' do
       let(:file) { fixture_file_upload('spec/fixtures/img.png', 'image/png') }
 
-      it { expect(subject.valid?).to be false }
+      it { expect(parser.valid?).to be false }
     end
 
     context 'quando o arquivo está vazio' do
       let(:file) { fixture_file_upload('spec/fixtures/empty.txt', 'txt') }
 
-      it { expect(subject.valid?).to be false }
+      it { expect(parser.valid?).to be false }
     end
   end
 end
