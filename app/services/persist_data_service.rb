e4c63dfca3e5bbb487dd7443264bc1d411ebef94
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-16 10:40:49 -0300

    renomeia serviços

---
+++ app/services/persist_data_service.rb
@@ -0,0 +1,19 @@
+class PersistDataService
+  def initialize(data:, user:)
+    @data = data
+    @user = user
+  end
+
+  def call
+    cnab_file = CnabFile.create(user: @user)
+
+    @data.each do |t|
+      store = Store.find_or_create_by(owner: t[:store][:owner], 
+                                      name: t[:store][:name], 
+                                      cnab_file: cnab_file)
+      Transaction.create(t.except!(:store).merge({store_id: store.id}))
+    end
+
+    cnab_file
+  end
+end
