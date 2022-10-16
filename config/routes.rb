02f80db119267b00bc7d848665bc426b75271c1f
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-15 23:17:24 -0300

    organiza routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root 'home#index'
-  post :parse_cnab, controller: :home
+  devise_for :users
   resources :cnab_file, only: %i[show create destroy]
 end
