68d19dfab8ea24b2fa56a7c88bbd1395de2eec4a
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-15 22:41:49 -0300

    remove warnings rubocop

--- app/models/cnab_file.rb
+++ app/models/cnab_file.rb
@@ -1,5 +1,5 @@
 class CnabFile < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :stores
+  has_many :stores, dependent: :destroy
   has_many :transactions, through: :stores
 end
--- spec/factories/cnab_files.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :cnab_file do
-    
-  end
-end
