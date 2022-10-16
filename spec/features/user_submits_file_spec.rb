4dbbcefb14a8ece2badc64ca1bc2700368c6996e
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-10-16 19:18:11 -0300

    Feature - Usuário deleta arquivo (#13)

---
+++ spec/features/user_submits_file_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'User submits file to be parsed', type: :feature do
+  let(:user) { create(:user) }
+  let(:file) { Rails.root.join('spec/fixtures/CNAB.txt') }
+
+  scenario 'successfully' do
+    sign_in(user)
+
+    visit root_path
+    page.attach_file('file', file)
+    click_on :commit
+
+    expect(current_path).to eq cnab_file_path(CnabFile.last)
+    expect(page).to have_text('Arquivo')
+    expect(page).to have_link('Excluir')
+  end
+end
