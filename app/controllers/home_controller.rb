7d501e12bfae7de77236abbed61c6053f1ffe863
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-15 22:57:29 -0300

    ordena arquivos na página inicial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @cnabs = current_user.cnab_files
+    @cnabs = current_user.cnab_files.order(:created_at)
   end
 end
