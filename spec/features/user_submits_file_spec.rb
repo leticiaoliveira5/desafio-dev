090d21a0b403077270ab2ccdeaae92af4b0f7b4c
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-16 18:50:43 -0300

    corrige erros do teste

--- spec/features/user_submits_file_spec.rb
+++ spec/features/user_submits_file_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-describe 'User submits file', type: :feature do
+describe 'User submits file to be parsed', type: :feature do
   let(:user) { create(:user) }
-  let(:file) { Rails.root + 'spec/fixtures/CNAB.txt' }
+  let(:file) { "#{Rails.root}/spec/fixtures/CNAB.txt" }
 
   scenario 'successfully' do
     sign_in(user)
 
     visit root_path
     page.attach_file('file', file)
-    click_on :submit
+    click_on :commit
 
     expect(current_path).to eq cnab_file_path(CnabFile.last)
     expect(current_path).to have_content('Arquivo')
