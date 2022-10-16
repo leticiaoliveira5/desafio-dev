3fc69d0d6728a2c2fe15d878d0b3c0e86878d285
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-16 13:50:55 -0300

    reorganiza lógica do upload do arquivo na api

--- app/controllers/api/v1/cnab_controller.rb
+++ app/controllers/api/v1/cnab_controller.rb
@@ -2,21 +2,21 @@ module Api
   module V1
     class CnabController < Api::V1::ApiController
       def parse_cnab
-        return unless params[:file]
+        data = ParseFileService.new(file: params[:file]).call
 
-        data = ParseFileService.new(file: params[:file].tempfile).call
-        render json: { error: 'The file count not be parsed' } unless data
-
-        cnab_file = PersistDataService.new(data: data, user: current_user).call
-
-        if cnab_file
-          render json: cnab_file.to_json(
-            include: { stores: { include: {
-              transactions: { except: %i[created_at updated_at store_id] }
-            }, only: %i[id name owner] } }, only: :id
-          ), status: :created
+        if data
+          cnab_file = PersistDataService.new(data: data, user: current_user).call
+          if cnab_file
+            render json: cnab_file.to_json(
+              include: { stores: { include: {
+                transactions: { except: %i[created_at updated_at store_id] }
+              }, only: %i[id name owner] } }, only: :id
+            ), status: :created
+          else
+            render json: { error: 'Data could not be persisted' }, status: :unprocessable_entity
+          end
         else
-          render json: { error: 'Data could not be persisted' }, status: :unprocessable_entity
+          render json: { error: 'The file count not be parsed' }
         end
       end
     end
