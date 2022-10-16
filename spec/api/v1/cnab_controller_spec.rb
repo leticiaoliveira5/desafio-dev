2d984eda109ec85c58b91a673c022e52eb637188
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-16 11:33:25 -0300

    implementa teste para api

---
+++ spec/api/v1/cnab_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Api::V1::CnabController, type: :request do
+  let(:file) { fixture_file_upload('spec/fixtures/CNAB.txt', 'text/plain') }
+
+  context 'parse_cnab' do
+    it 'interpreta o arquivo e retorna os objetos criados' do
+      params = { file: file, format: 'json' }
+      headers = { 'Content-Type': 'text/plain' }
+
+      post api_v1_parse_cnab_path(params), headers: headers
+
+      expect(response).to have_http_status(201)
+    end
+  end
+end
