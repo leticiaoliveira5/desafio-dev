de7bb70909f0f1058e0c06df1fddd04f61b1ce1d
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-15 22:38:59 -0300

    remove lógica de parseamento do homecontroller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @stores = current_user.stores.includes(:transactions)
-  end
-
-  def parse_cnab
-    return unless params[:file]
-
-    parser = Parser.new(file: params[:file], current_user: current_user)
-
-    if parser.valid? && parser.call
-      redirect_to root_path, notice: t('.parser_success')
-    else
-      redirect_to root_path, alert: t('.parser_failure')
-    end
+    @cnabs = current_user.cnab_files
   end
 end
