ba60451af37fcf44294320aaefac54c31a033be6
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-15 21:52:25 -0300

    cria relação entre os models

--- app/models/cnab_file.rb
+++ app/models/cnab_file.rb
@@ -1,2 +1,5 @@
 class CnabFile < ApplicationRecord
+  belongs_to :user, optional: true
+  has_many :stores
+  has_many :transactions, through: :stores
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,6 @@
 class Store < ApplicationRecord
   belongs_to :user
+  belongs_to :cnab_file
   has_many :transactions, dependent: :destroy
 
   validates :owner, :name, presence: true
