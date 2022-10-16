aaf61f12238bf4bf774a0bed973b257386af24e7
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-15 22:38:41 -0300

    descreve action create do arquivo

--- app/controllers/cnab_file_controller.rb
+++ app/controllers/cnab_file_controller.rb
@@ -3,16 +3,22 @@ class CnabFileController < ApplicationController
 
   def show
     @cnab_file = CnabFile.find(params[:id])
-    @stores = cnab_file.stores.includes(:transactions)
+    @stores = @cnab_file.stores.includes(:transactions)
   end
 
-  def create(cnab_file_params)
-    Parser.new(file: file, user: cnab_file.user)
+  def create
+    parser = Parser.new(file: params[:file], current_user: current_user)
+
+    if parser.valid? && parser.call
+      redirect_to root_path, notice: t('.parser_success')
+    else
+      redirect_to root_path, alert: t('.parser_failure')
+    end
   end
 
   private
 
   def cnab_file_params
-    
+    params.require(:company).permit(:file, :user)
   end
 end
