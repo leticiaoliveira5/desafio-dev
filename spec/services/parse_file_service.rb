183251cc007eae36e973eb8708ed9bc1c8e32bb2
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-16 10:40:28 -0300

    atualiza testes com novos serviços

--- spec/services/parse_file_service.rb
+++ spec/services/parse_file_service.rb
@@ -1,18 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Parser, type: :service do
-  let(:user) { create(:user) }
+RSpec.describe ParseFileService, type: :service do
   let(:file) { fixture_file_upload('spec/fixtures/CNAB.txt', 'text/plain') }
-  let(:parser) { described_class.new(file: file, current_user: user) }
-
-  describe '#call' do
-    it 'insere os dados nas tabelas loja e transação' do
-      expect { parser.call }.to change(Transaction, :count).by(21)
-        .and change(Store, :count).by(5)
-        .and change(user.cnab_files, :count).by(1)
-      expect(Transaction.first.type).to eq 'financing'
-    end
-  end
+  let(:parser) { described_class.new(file: file) }
 
   describe '#valid?' do
     context 'quando o formato do arquivo é txt' do
